18cec53a22fa181a5b9a46979833ff8145edcfc0
David Tanner Jr <tannerdjr@gmail.com>
2025-03-09 16:39:14 -0400

    fix: updated cdk sciprt to allow access to ecs, and updated healthcheck

--- lib/go-api-infra-stack.ts
+++ lib/go-api-infra-stack.ts
@@ -1,16 +1,54 @@
+
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class GoApiInfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'GoApiVpc', { maxAzs: 2 });
+    const cluster = new ecs.Cluster(this, 'GoApiCluster', { vpc: vpc });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GoApiInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'GoApiFargateService', {
+      cluster: cluster,
+      cpu: 256,
+      memoryLimitMiB: 512,
+      desiredCount: 1,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('069597727371.dkr.ecr.us-east-1.amazonaws.com/go-api:latest'),
+        containerPort: 8080,
+      },
+      publicLoadBalancer: true,
+    });
+
+    // Update the ALB health check to use /api/healthcheck instead of the default "/"
+    fargateService.targetGroup.configureHealthCheck({
+      path: '/api/healthcheck',
+      interval: cdk.Duration.seconds(30),
+      timeout: cdk.Duration.seconds(5),
+      healthyThresholdCount: 2,
+      unhealthyThresholdCount: 5,
+    });
+
+    fargateService.taskDefinition.addToExecutionRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ],
+        resources: ["*"],
+      })
+    );
+
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
+      value: fargateService.loadBalancer.loadBalancerDnsName,
+    });
   }
 }
