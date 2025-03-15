12945cedf715b7e88e1df50cf26d046b69b98234
David Tanner Jr <tannerdjr@gmail.com>
2025-03-15 17:09:01 -0400

    testing

--- lib/go-api-prod-infra-stack.ts
+++ lib/go-api-prod-infra-stack.ts
@@ -1,5 +1,4 @@
 import * as cdk from 'aws-cdk-lib';
-
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
@@ -10,10 +9,29 @@ export class GoApiProdInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create a VPC with high availability (3 AZs for production)
+    // Lookup the default VPC
     const vpc = ec2.Vpc.fromLookup(this, 'ExistingVpc', {
-      isDefault: true, // or specify VPC ID or tags
 
+      isDefault: true,
+    });
+
+    // Add VPC endpoints so tasks in private subnets can access necessary AWS services
+    vpc.addInterfaceEndpoint('EcrApiEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR,
+    });
+
+    vpc.addInterfaceEndpoint('EcrDockerEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+    });
+
+    vpc.addGatewayEndpoint('S3Endpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.S3,
+    });
+
+
+    // Add a VPC endpoint for CloudWatch Logs to enable logging without public internet access
+    vpc.addInterfaceEndpoint('CloudWatchLogsEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
     });
 
     // Create the ECS Cluster within the VPC
@@ -25,9 +43,12 @@ export class GoApiProdInfraStack extends cdk.Stack {
       cpu: 512,                // Increased CPU allocation
       memoryLimitMiB: 1024,    // Increased memory allocation
       desiredCount: 2,         // More tasks for high availability
+
       taskImageOptions: {
+
         image: ecs.ContainerImage.fromRegistry('069597727371.dkr.ecr.us-east-1.amazonaws.com/go-api:latest'),
         containerPort: 8080,
+
       },
       publicLoadBalancer: true,
     });
@@ -37,7 +58,6 @@ export class GoApiProdInfraStack extends cdk.Stack {
     fargateService.targetGroup.configureHealthCheck({
       path: '/api/healthcheck',
       interval: cdk.Duration.seconds(30),
-
       timeout: cdk.Duration.seconds(5),
       healthyThresholdCount: 2,
       unhealthyThresholdCount: 5,
@@ -47,32 +67,31 @@ export class GoApiProdInfraStack extends cdk.Stack {
     const scaling = fargateService.service.autoScaleTaskCount({ maxCapacity: 5 });
     scaling.scaleOnCpuUtilization('CpuScaling', {
       targetUtilizationPercent: 50,
-
       scaleInCooldown: cdk.Duration.seconds(60),
       scaleOutCooldown: cdk.Duration.seconds(60),
+
     });
 
     // Allow ECS tasks to pull images from ECR
     fargateService.taskDefinition.addToExecutionRolePolicy(
       new iam.PolicyStatement({
         effect: iam.Effect.ALLOW,
-
         actions: [
           "ecr:GetAuthorizationToken",
           "ecr:BatchCheckLayerAvailability",
+
           "ecr:GetDownloadUrlForLayer",
           "ecr:BatchGetImage"
+
         ],
-
         resources: ["*"],
-
       })
     );
 
     // Output the load balancer DNS so you can easily access the service
     new cdk.CfnOutput(this, 'LoadBalancerDNS', {
       value: fargateService.loadBalancer.loadBalancerDnsName,
+
     });
   }
 }
-
