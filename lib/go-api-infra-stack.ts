e0bc8ff60ace109df3c95e88447d47e1771fa290
David Tanner Jr <tannerdjr@gmail.com>
2025-05-18 18:33:36 -0400

    added lambda

--- lib/go-api-infra-stack.ts
+++ lib/go-api-infra-stack.ts
@@ -5,6 +5,7 @@ import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class GoApiInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,10 +16,24 @@ export class GoApiInfraStack extends cdk.Stack {
     // Creating the ECS cluster
     const cluster = new ecs.Cluster(this, 'GoApiCluster', { vpc: vpc });
 
+    // Create a simple Lambda function
+    const processDataLambda = new lambda.Function(this, 'ProcessDataLambda', {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+def handler(event, context):
+    print("Received event:", event)
+    return {
+        'statusCode': 200,
+        'body': 'Event received successfully!'
+    }
+      `),
+    });
+
     // Reference existing certificate
     const certificate = acm.Certificate.fromCertificateArn(
-      this, 
-      'APICertificate', 
+      this,  
+      'APICertificate',  
       'arn:aws:acm:us-east-1:069597727371:certificate/146132c0-6175-4ce3-8edf-0d4108d53287'
     );
 
@@ -70,9 +85,31 @@ export class GoApiInfraStack extends cdk.Stack {
       })
     );
 
+    // Grant the task role permissions to call EventBridge
+    fargateService.taskDefinition.taskRole.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          'events:PutRule',    // create/update rules
+          'events:PutTargets', // attach targets to rules
+          // optionally add 'events:DescribeRule', 'events:DeleteRule', etc.
+        ],
+        resources: [
+          // scope as narrowly as you like:
+          // 'arn:aws:events:us-east-1:069597727371:rule/my-scheduled-event',
+          'arn:aws:events:us-east-1:069597727371:rule/*',
+        ],
+      })
+    );
+
     new cdk.CfnOutput(this, 'LoadBalancerDNS', {
       value: fargateService.loadBalancer.loadBalancerDnsName,
     });
+
+    // Output the Lambda ARN
+    new cdk.CfnOutput(this, 'LambdaARN', {
+      value: processDataLambda.functionArn,
+      description: 'ARN of the Process Data Lambda function',
+    });
   }
 }
-
